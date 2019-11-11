b610daac619ee4610b698f761a2880bf99f51081
cecisousa <ceciliasousa21@gmail.com>
2019-11-11 00:03:07 -0200

    Initial commit

---
+++ app/src/main/java/com/example/dh_marvelapp/DetalheActivity.java
@@ -0,0 +1,14 @@
+package com.example.dh_marvelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+    }
+}
---
+++ app/src/main/java/com/example/dh_marvelapp/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.dh_marvelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Timer timer = new Timer();
+    private ImageView splash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        splash = findViewById(R.id.imageSplash);
+
+        splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jump();
+            }
+        });
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                jump();
+            }
+        },3000);
+    }
+
+    private void jump(){
+        timer.cancel();
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
